614b0004d3e8eae10d546fda6b99cb094b2a823d
pavelsokolovan <pavel.sokolov.an@gmail.com>
2015-07-07 22:53:43 +0300

    Add data validation on Win Form

--- MyLabClient/Form1.Designer.cs
+++ MyLabClient/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonTubeSave = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxTubeCode = new System.Windows.Forms.TextBox();
+            this.textBoxTubeName = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxTubeVolume = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -48,15 +48,16 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Tube Setup";
             // 
-            // button1
+            // buttonTubeSave
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(144, 133);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonTubeSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTubeSave.Location = new System.Drawing.Point(144, 133);
+            this.buttonTubeSave.Name = "buttonTubeSave";
+            this.buttonTubeSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonTubeSave.TabIndex = 1;
+            this.buttonTubeSave.Text = "Save";
+            this.buttonTubeSave.UseVisualStyleBackColor = true;
+            this.buttonTubeSave.Click += new System.EventHandler(this.buttonTubeSave_Click);
             // 
             // label2
             // 
@@ -68,21 +69,22 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Code:";
             // 
-            // textBox1
+            // textBoxTubeCode
             // 
-            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.textBox1.Location = new System.Drawing.Point(74, 46);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(145, 20);
-            this.textBox1.TabIndex = 3;
+            this.textBoxTubeCode.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxTubeCode.Location = new System.Drawing.Point(74, 46);
+            this.textBoxTubeCode.Name = "textBoxTubeCode";
+            this.textBoxTubeCode.Size = new System.Drawing.Size(145, 20);
+            this.textBoxTubeCode.TabIndex = 3;
+            this.textBoxTubeCode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxTubeCode_KeyPress);
+            this.textBoxTubeCode.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxEmpty_Validating);            
             // 
-            // textBox2
+            // textBoxTubeName
             // 
-            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.textBox2.Location = new System.Drawing.Point(74, 72);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(145, 20);
-            this.textBox2.TabIndex = 5;
+            this.textBoxTubeName.Location = new System.Drawing.Point(74, 72);
+            this.textBoxTubeName.Name = "textBoxTubeName";
+            this.textBoxTubeName.Size = new System.Drawing.Size(145, 20);
+            this.textBoxTubeName.TabIndex = 5;
             // 
             // label3
             // 
@@ -94,13 +96,14 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Name:";
             // 
-            // textBox3
+            // textBoxTubeVolume
             // 
-            this.textBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.textBox3.Location = new System.Drawing.Point(74, 98);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(145, 20);
-            this.textBox3.TabIndex = 7;
+            this.textBoxTubeVolume.Location = new System.Drawing.Point(74, 98);
+            this.textBoxTubeVolume.MaxLength = 3;
+            this.textBoxTubeVolume.Name = "textBoxTubeVolume";
+            this.textBoxTubeVolume.Size = new System.Drawing.Size(145, 20);
+            this.textBoxTubeVolume.TabIndex = 7;
+            this.textBoxTubeVolume.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxTubeVolume_KeyPress);
             // 
             // label4
             // 
@@ -117,13 +120,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(249, 172);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxTubeVolume);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxTubeName);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxTubeCode);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonTubeSave);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "MyLab";
@@ -135,12 +138,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonTubeSave;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxTubeCode;
+        private System.Windows.Forms.TextBox textBoxTubeName;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxTubeVolume;
         private System.Windows.Forms.Label label4;
 
     }
--- MyLabService/Services/TubeCollector.cs
+++ MyLabService/Services/TubeCollector.cs
@@ -15,36 +15,22 @@ namespace MyLabService.Services
         // Return false - if new row wasn't added
         public bool Add(string code, string name, int volume)
         {
-            // Connection to DB
-            SqlConnection connection = new SqlConnection(
-                @"Data Source=.\SQLEXPRESS;" +
-                @"Initial Catalog=MyLabDB1;" +
-                //@"AttachDbFilename='C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\MyLabDB1.mdf';" +   
-                @"Integrated Security=sspi;Connect Timeout=30;");
-            //@"User Instance=true;");
+            // Open connection to DB
+            SqlConnection connection = this.CreateSqlConnection();
             connection.Open();
 
-            // Create DataAdapter
-            SqlDataAdapter dataAdapter = new SqlDataAdapter(
-                "select * from TUBE", connection);
-            // Set property for download primery keys
-            dataAdapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
-
-            // Create CommandBuilder
-            SqlCommandBuilder builder = new SqlCommandBuilder(dataAdapter);
-
-            // Create DataSet
-            DataSet dataSet = new DataSet();
-
-            // Fill DataSet
-            dataAdapter.Fill(dataSet, "Table");
-
+            // Create DataSet with data from DB
+            DataSet dataSet;
+            SqlDataAdapter dataAdapter;
+            string tableName = "Table";
+            GetDataSet("select * from TUBE", connection, tableName, out dataSet, out dataAdapter);
+   
             // Check if new code is exist in DB            
-            DataRow findRow = dataSet.Tables["Table"].Rows.Find(code);  // Try to find row with new code            
+            DataRow findRow = dataSet.Tables[tableName].Rows.Find(code);  // Try to find row with new code            
             if (findRow == null)    // new code isn't exist in DB - add new row
             {
                 // Create new Row
-                DataRow newRow = dataSet.Tables["Table"].NewRow();
+                DataRow newRow = dataSet.Tables[tableName].NewRow();
                 // Fill new Row with data
                 //newRow["TUBE_ID"] = ;
                 newRow["TUBE_CODE"] = code;
@@ -55,7 +41,7 @@ namespace MyLabService.Services
                 dataSet.Tables["Table"].Rows.Add(newRow);
 
                 // Update Table in DB
-                dataAdapter.Update(dataSet, "Table");
+                dataAdapter.Update(dataSet, tableName);
 
                 // Close
                 connection.Close(); 
@@ -71,30 +57,65 @@ namespace MyLabService.Services
                 // Close
                 connection.Close();
                 return false;
-            }                  
+            }
         }
 
-        /* Method to get collection of tube codes
-        public string GetTubeCodes()
+        // Method to checks if code is presented in DB
+        // Return true - if code is presented in DB
+        // Return false - if code isn't presented in DB
+        public bool Contains(string code)
         {
-            string[] array = new string[tubes.Count];
-            Console.WriteLine(tubes.Count);
-            int i = 0;
-            foreach(string item in tubes.Keys)
-            {
-                array[i] = item;
-            }
-            string st2 = array[0];
-            Console.WriteLine(st2);
-            return "yee";
-        }*/
+            // Open connection to DB
+            SqlConnection connection = this.CreateSqlConnection();
+            connection.Open();
 
-        /*/ TEMP
-        public string[] GetString(string st)
+            // Create DataSet with data from DB
+            DataSet dataSet;
+            SqlDataAdapter dataAdapter;
+            string tableName = "Table";
+            GetDataSet("select TUBE_CODE from TUBE", connection, tableName, out dataSet, out dataAdapter);
+
+            // Try to find code in dataSet
+            DataRow findRow = dataSet.Tables["Table"].Rows.Find(code);
+
+            // Close connection
+            connection.Close();
+
+            return (findRow != null);
+        }
+
+        // Method Create Sql Connection
+        // Return SqlConnection instance
+        private SqlConnection CreateSqlConnection()
         {
-            /*Dictionary<string, string> dic = new Dictionary<string,string>;
-            dic.Add(st, st);
-            return new string[] {st};
-        }*/
+            // Connection to DB
+            SqlConnection connection = new SqlConnection(
+                @"Data Source=.\SQLEXPRESS;" +
+                @"Initial Catalog=MyLabDB1;" +
+                //@"AttachDbFilename='C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\MyLabDB1.mdf';" +   
+                @"Integrated Security=sspi;Connect Timeout=30;");
+            //@"User Instance=true;");
+                        
+            return connection;
+        }
+
+        // Method Get DataSet
+        private void GetDataSet(string commandText, SqlConnection connection, string tableName, out DataSet dataSet, out SqlDataAdapter dataAdapter)
+        {
+            // Create DataAdapter
+            dataAdapter = new SqlDataAdapter(
+                commandText, connection);
+            // Set property for download primery keys
+            dataAdapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+
+            // Create CommandBuilder
+            SqlCommandBuilder builder = new SqlCommandBuilder(dataAdapter);
+
+            // Create DataSet
+            dataSet = new DataSet();
+
+            // Fill DataSet
+            dataAdapter.Fill(dataSet, tableName);
+        }
     }
 }
